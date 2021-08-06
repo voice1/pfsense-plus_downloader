2e6dcf1266d96f37136492f510546a70631991c3
VOICE1 <voice1me@gmail.com>
2021-08-06 12:07:30 -0700

    Updated current_version
    Updated CURRENT_VERSION and fixed default values for `read` command.

--- batch-downloadr.sh
+++ batch-downloadr.sh
@@ -6,7 +6,7 @@
 
 
 USER_COOKIE=""
-CURRENT_VERISON="21.05"
+CURRENT_VERISON="21.05.1"
 DL_CHECKSUM=1		# Set to 1 to download .sha256 checksum
 VERIFY_CHECKSUM=0	# Set to 0 to verify checksum.
 CURL_OPTS="--compressed --progress-bar -O"
@@ -23,19 +23,19 @@ function verify_sha256() {
 function download() {
 
 	# SG-1000
-	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1000/pfSense-plus-SG-1000-recover-${CURRENT_VERISON}-RELEASE-armv7.img.gz"
+	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1000/pfSense-plus-SG-1000-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
 	# SG-1100
-	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1100/pfSense-plus-SG-1100-recover-${CURRENT_VERISON}-RELEASE-armv7.img.gz"
+	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1100/pfSense-plus-SG-1100-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
 	# SG-2100
-	URL[5]="https://partnervault.netgate.com/files/firmware/sg-2100/pfSense-plus-SG-2100-recovery-${CURRENT_VERISON}-RELEASE-aarch64.img.gz"
+	URL[5]="https://partnervault.netgate.com/files/firmware/sg-2100/pfSense-plus-SG-2100-recovery-${DOWNLOAD_VERISON}-RELEASE-aarch64.img.gz"
 	# SG-3100
-	URL[4]="https://partnervault.netgate.com/files/firmware/sg-3100/pfSense-plus-SG-3100-recover-${CURRENT_VERISON}-RELEASE-armv7.img.gz"
+	URL[4]="https://partnervault.netgate.com/files/firmware/sg-3100/pfSense-plus-SG-3100-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
 
 	# memstick
-	URL[0]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-${CURRENT_VERISON}-RELEASE-amd64.img.gz"
-	URL[1]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-ADI-${CURRENT_VERISON}-RELEASE-amd64.img.gz"
-	URL[2]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-XG-7100-${CURRENT_VERISON}-RELEASE-amd64.img.gz"
-	URL[3]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-serial-${CURRENT_VERISON}-RELEASE-amd64.img.gz"
+	URL[0]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	URL[1]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-ADI-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	URL[2]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-XG-7100-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	URL[3]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-serial-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
 
 	# Download files.
 	for url in "${URL[@]}"
@@ -61,12 +61,11 @@ function download() {
 	done
 }
 
-read -p "Enter the pfsense+ version to download: " CURRENT_VERISON
-CURRENT_VERISON=${CURRENT_VERISON:-20.05}
+read -p "Enter the pfsense+ version to download [${CURRENT_VERISON}]: " DOWNLOAD_VERISON
+DOWNLOAD_VERISON=${DOWNLOAD_VERISON:-${CURRENT_VERISON}}
 
 read -p "Paste your user cookie, or enter to use default: " COOKIE
-COOKIE=${COOKIE:-USER_COOKIE}
-
+COOKIE=${COOKIE:-${USER_COOKIE}}
 download
 
 echo "Done. Have a nice day!"
