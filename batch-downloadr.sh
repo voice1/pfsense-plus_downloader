93c88b8b51247e9d37509c4128aa50b235666efa
VOICE1 <voice1me@gmail.com>
2022-03-03 11:06:08 -0800

    Updated to support new firmwar filenames
    Updated to support the change of firmware filenames and a new structure for firmware storage.

--- batch-downloadr.sh
+++ batch-downloadr.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
+_verison = 0.0.2
 
 # Download pfsense+ firmware from site.
 # Requires cookies to be valid first so a browswer login and  fetch of the cookie is required.
 # Setting version allows for downloading only current version.
 
+# CHANGES
+# * 3/3/22 - Netgate updated filenames to include 'Netgate' and dropped the 'SG' prefix.
+# * 3/3/22 - firmware neme for '-recover-' changed to '-recovery-'
+# * 3/3/22 - Added prompt to first login to partner portal and copy cookie.
 
 USER_COOKIE=""
-CURRENT_VERISON="21.05.1"
+CURRENT_VERISON="22.01"
 DL_CHECKSUM=1		# Set to 1 to download .sha256 checksum
 VERIFY_CHECKSUM=0	# Set to 0 to verify checksum.
 CURL_OPTS="--compressed --progress-bar -O"
+
+function sort_firmware_versions() {
+	# Sort firmware to directories by model type.
+	echo "Not implemented"
+}
 
 function verify_sha256() {
 	# Verify SHA256 signature Filename should not have the .sha256 suffix
@@ -21,21 +31,25 @@ function verify_sha256() {
 }
 
 function download() {
-
-	# SG-1000
-	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1000/pfSense-plus-SG-1000-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
+	# Starting on 22.01 Netgate started adding "Netgate-" to the filenames and dropped the SG- but kept the sg in the path.
+	# Additionally they renamed -recover- to -recovery-
+	BRANDING="pfSense-plus-Netgate"
 	# SG-1100
-	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1100/pfSense-plus-SG-1100-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
+	URL[0]="https://partnervault.netgate.com/files/firmware/sg-1100/${BRANDING}-1100-recovery-${DOWNLOAD_VERISON}-RELEASE-aarch64.img.gz"
 	# SG-2100
-	URL[5]="https://partnervault.netgate.com/files/firmware/sg-2100/pfSense-plus-SG-2100-recovery-${DOWNLOAD_VERISON}-RELEASE-aarch64.img.gz"
+	URL[1]="https://partnervault.netgate.com/files/firmware/sg-2100/${BRANDING}-2100-recovery-${DOWNLOAD_VERISON}-RELEASE-aarch64.img.gz"
 	# SG-3100
-	URL[4]="https://partnervault.netgate.com/files/firmware/sg-3100/pfSense-plus-SG-3100-recover-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
-
-	# memstick
-	URL[0]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
-	URL[1]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-ADI-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
-	URL[2]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-XG-7100-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
-	URL[3]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-serial-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	URL[2]="https://partnervault.netgate.com/files/firmware/sg-3100/${BRANDING}-3100-recovery-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
+	# SG-5100-6100
+	URL[3]="https://partnervault.netgate.com/files/firmware/sg-5100-6100/pfSense-plus-memstick-serial-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	# SG-7100
+	URL[4]="https://partnervault.netgate.com/files/firmware/sg-7100/pfSense-plus-memstick-Netgate-7100-${DOWNLOAD_VERSION}-RELEASE-amd64.img.gz"
+	# SG-1537/1541 aka. memstick
+	URL[5]="https://partnervault.netgate.com/files/firmware/memstick/pfSense-plus-memstick-${DOWNLOAD_VERISON}-RELEASE-amd64.img.gz"
+	# SG-1000
+	URL[6]="https://partnervault.netgate.com/files/firmware/sg-1000/pfSense-plus-SG-1000-recovery-${DOWNLOAD_VERISON}-RELEASE-armv7.img.gz"
+	# SG-2220-2440-4869-8860-XG-275 (EOS)
+	URL[7]="https://partnervault.netgate.com/files/firmware/sg-2220-2440-4860-8860-xg-2758/pfSense-plus-memstick-ADI-SG-2220-2440-4860-8860-XG-2758-${DOWNLOAD_VERSION}-RELEASE-amd64.img.gz"
 
 	# Download files.
 	for url in "${URL[@]}"
@@ -61,11 +75,16 @@ function download() {
 	done
 }
 
+read -p "Login to https://partnervault.netgate.com and copy the cookie to the clipboard. Press enter to continue."
+
 read -p "Enter the pfsense+ version to download [${CURRENT_VERISON}]: " DOWNLOAD_VERISON
 DOWNLOAD_VERISON=${DOWNLOAD_VERISON:-${CURRENT_VERISON}}
 
-read -p "Paste your user cookie, or enter to use default: " COOKIE
+read -p "Paste your user cookie, or enter to use default: (uf4=) " COOKIE
 COOKIE=${COOKIE:-${USER_COOKIE}}
+
 download
 
 echo "Done. Have a nice day!"
+
+# https://everything.curl.dev/usingcurl/copyas
